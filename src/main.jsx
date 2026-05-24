e1ff8cb1a0a59a68c31685b257427a78eb05ee18
ronnelcuevas143-design <ronnelcuevas143@gmail.com>
2026-05-24 16:42:41 +0800

    Create src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FVGScanner from './FVGScanner.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FVGScanner />
+  </React.StrictMode>
+)
