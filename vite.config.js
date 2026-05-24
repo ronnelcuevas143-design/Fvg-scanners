3189d1d98a1a4e2be6edeff9b0569dd6e6d63f2d
ronnelcuevas143-design <ronnelcuevas143@gmail.com>
2026-05-24 17:05:33 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'fvg-scanner' with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/fvg-scanner/',
+  base: '/Fvg-scanners/',
 })
